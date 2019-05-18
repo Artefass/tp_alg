93dd760f7d77250d47f9821bed18811c58ee6629
Artefas <pasha777kot@mail.ru>
2019-05-18 12:34:15 +0300

    поправил баг в конструкторе копирования CMatrixGraph

---
+++ mod3/lab2/CListGraph.h
@@ -0,0 +1,8 @@
+//
+// Created by pasha on 18.05.2019.
+//
+
+#ifndef LAB2_CLISTGRAPH_H
+#define LAB2_CLISTGRAPH_H
+
+#endif //LAB2_CLISTGRAPH_H
---
+++ mod3/lab2/IGraph.h
@@ -0,0 +1,8 @@
+//
+// Created by pasha on 18.05.2019.
+//
+
+#ifndef LAB2_IGRAPH_H
+#define LAB2_IGRAPH_H
+
+#endif //LAB2_IGRAPH_H
